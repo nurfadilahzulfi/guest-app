62ba000936c31e29de1126296e92d37ad0d609ed
nurfadilahzulfi <nurfadilahzulfi@gmail.com>
2026-09-12 09:23:35 +0700

    configure Vercel deployment with Prisma generate and NextAuth v5

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["192.168.1.91", "localhost", "127.0.0.1"],
+  allowedDevOrigins: ["192.168.1.161", "localhost", "127.0.0.1"],
   images: {
     unoptimized: true,
   },
